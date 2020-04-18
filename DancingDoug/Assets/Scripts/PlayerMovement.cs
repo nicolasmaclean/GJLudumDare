916f77c145a56ed54b239279c4c47d71092c3b2b
Nick <42703997+nicolasmaclean@users.noreply.github.com>
2020-04-17 20:21:18 -0500

    initialized project and setup player controller

---
+++ DancingDoug/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D characterController;
+    public float moveSpeed = 40f;
+
+    float horizontalInput;
+    bool jump;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        jump = Input.GetButtonDown("Jump");
+        Debug.Log(jump);
+    }
+
+    void FixedUpdate()
+    {
+        characterController.Move(horizontalInput * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
